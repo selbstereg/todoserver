7f70566dbcd36c62fe6ab7ab7a28c30c1a63ba34
Cornelius Goldbeck <cornelius.goldbeck@andrena.de>
2020-04-01 14:15:44 +0200

    Added jpa compiler plugin for no-arg constructors.

--- src/main/kotlin/todoappbackend/todoserver/todolist/ToDoList.kt
+++ src/main/kotlin/todoappbackend/todoserver/todolist/ToDoList.kt
@@ -4,15 +4,8 @@ import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
 
-// TODO: On App startup, Hibernate complains, that the getter for property "name" is final, as it
-// need to override it for lazy initialization. What should this class look like, so we benefit
-// from null safety, JPA is happy and it is hard for a developer
-// to instantiate an uninitialized instance?
 @Entity
-open class ToDoList(val name: String) {
-    // For JPA provider
-    protected constructor() : this("")
-
+data class ToDoList(val name: String) {
     @Id
     @GeneratedValue
     val id: Long? = null
