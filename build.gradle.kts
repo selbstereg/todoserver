43513939d0f4847a0c181f267d3166a7caa96fe0
Paul Bauknecht <paul.bauknecht@andrena.de>
2020-04-01 10:21:56 +0200

    Configure postgres db connection

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,10 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
