a094e08602a39e3bc6ab63458fa0b2adda91586b
Paul Bauknecht <paul.bauknecht@andrena.de>
2020-04-01 10:26:55 +0200

    Remove redundant interface

--- src/main/kotlin/todoappbackend/todoserver/service/ToDoListCrudService.kt
+++ src/main/kotlin/todoappbackend/todoserver/service/ToDoListCrudService.kt
@@ -1,8 +1,11 @@
 package todoappbackend.todoserver.service
 
+import org.springframework.stereotype.Component
 import todoappbackend.todoserver.model.ToDoList
 
-interface ToDoListCrudService {
-    fun getToDoLists(): Collection<ToDoList>
+@Component
+class ToDoListCrudService {
+    fun getToDoLists(): Collection<ToDoList> {
+        return listOf(ToDoList("Einkaufsliste"))
+    }
 }
-
