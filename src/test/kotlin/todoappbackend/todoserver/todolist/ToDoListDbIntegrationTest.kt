0560ca450f66a54cde6d07734f5d176db85714c3
Cornelius Goldbeck <cornelius.goldbeck@andrena.de>
2020-04-03 10:24:17 +0200

    Make test more readable with TestEntityManager.

--- src/test/kotlin/todoappbackend/todoserver/todolist/ToDoListDbIntegrationTest.kt
+++ src/test/kotlin/todoappbackend/todoserver/todolist/ToDoListDbIntegrationTest.kt
@@ -1,30 +1,28 @@
 package todoappbackend.todoserver.todolist
 
 import org.amshove.kluent.`should contain`
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
-import org.springframework.data.repository.findByIdOrNull
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
 
 @DataJpaTest
 class ToDoListDbIntegrationTest {
 
     @Autowired
-    private lateinit var repo: ToDoListRepo
+    private lateinit var manager: TestEntityManager
 
     @Test
     fun `Should save a new ToDo when adding to an existing ToDo list`() {
         // TODO: Vielleicht darf man manager und repo nicht mischen, wegen hibernate transactions, lifecycle, ...
         val toDoList = ToDoList("TestList")
-        repo.saveAndFlush(toDoList)
-
+        manager.persistAndFlush(toDoList)
         val toDoToAdd = ToDo("TestToDo")
         toDoList.add(toDoToAdd)
-        repo.saveAndFlush(toDoList)
 
-        val retrievedToDoList = repo.findByIdOrNull(toDoList.id ?: Assertions.fail("Id was not set when persisting!"))
-        (retrievedToDoList ?: Assertions.fail("ToDoList was not found after persisting!")).todos `should contain` toDoToAdd
+        val persistedListWithToDo = manager.persistFlushFind(toDoList)
+
+        persistedListWithToDo.todos `should contain` toDoToAdd
     }
 
 }
