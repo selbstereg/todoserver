8332b8e22db258f52326a6f46e1f17cb310cfb3c
Mox Muriz <mox.muriz@hoelderlin.com>
2020-04-18 10:50:31 +0200

    Inline function to get rid of warning

--- src/test/kotlin/todoappbackend/todoserver/todolist/controller/ToDoListItemControllerIntegrationTest.kt
+++ src/test/kotlin/todoappbackend/todoserver/todolist/controller/ToDoListItemControllerIntegrationTest.kt
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.ResultActionsDsl
 import org.springframework.test.web.servlet.delete
 import org.springframework.test.web.servlet.post
 import todoappbackend.todoserver.todolist.ToDoListCrudService
@@ -49,17 +48,14 @@ class ToDoListItemControllerIntegrationTest {
         every { toDoCrudServiceMock.addToDo(any(), any()) } returns toDoToCreate
 
         val url = "/api/to-do-lists/${42L}/to-dos"
-        post(url, toDoToCreate)
+        mockMvc.post(url) {
+                    content = mapper.writeValueAsString(toDoToCreate)
+                    contentType = MediaType.APPLICATION_JSON
+                }
                 .andExpect {
                     status { isOk }
                     jsonPath("$.name", equalTo("TestToDo"))
                 }
     }
 
-    fun post(url: String, body: ToDo): ResultActionsDsl {
-        return mockMvc.post(url) {
-            content = mapper.writeValueAsString(body)
-            contentType = MediaType.APPLICATION_JSON
-        }
-    }
 }
