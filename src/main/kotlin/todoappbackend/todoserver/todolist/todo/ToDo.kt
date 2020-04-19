1ceb4f71ef81ffe7436af8ad5dda5fa8fac31714
Mox Muriz <mox.muriz@hoelderlin.com>
2020-04-19 21:09:44 +0200

    Add remark, that jackson should return 400 instead making up default values for missing to do properties

--- src/main/kotlin/todoappbackend/todoserver/todolist/todo/ToDo.kt
+++ src/main/kotlin/todoappbackend/todoserver/todolist/todo/ToDo.kt
@@ -9,6 +9,10 @@ import javax.persistence.Id
 // why we put the id into the primary constructor.
 // We provide the default value null, so you can still create a to do by only providing a name.
 
+// TODO Paul Bauknecht 19 Apr 2020: When jackson unmarshalls an incoming ToDo from json, and there is no
+// priority property, it just sets it to 0. Actually this should result in a 400 bad request, ideally
+// with an error message, that gives you a hint, what went wrong!
+
 @Entity
 data class ToDo(
         val name: String = "",
