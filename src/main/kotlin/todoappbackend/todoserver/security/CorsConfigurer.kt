d551a913b3564a806b49cb61645f2a80f0f6216f
Mox Muriz <mox.muriz@hoelderlin.com>
2020-04-19 21:27:14 +0200

    Add dedicated CORS allowed origins for dev and production

--- src/main/kotlin/todoappbackend/todoserver/security/CorsConfigurer.kt
+++ src/main/kotlin/todoappbackend/todoserver/security/CorsConfigurer.kt
@@ -1,5 +1,6 @@
 package todoappbackend.todoserver.security
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
@@ -8,12 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 @EnableWebMvc
 class CorsConfigurer : WebMvcConfigurer {
+
+    @Value("\${cors.allowed-origins}")
+    lateinit var allowedOrigins: Array<String>
+
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/api/**")
-                .allowedOrigins(
-                        "http://localhost:4200",
-                        "http://localhost:8080",
-                        "https://selbstereg-todo-app-frontend.herokuapp.com")
+                .allowedOrigins(*allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH")
         super.addCorsMappings(registry)
     }
