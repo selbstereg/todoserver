a7892c4a333ef6f34d133090f8be8acbcd3f4841
Mox Muriz <mox.muriz@hoelderlin.com>
2020-04-19 19:44:35 +0200

    Fix broken test

--- src/test/kotlin/todoappbackend/todoserver/utils/GlobalExceptionHandlerTestHelperController.kt
+++ src/test/kotlin/todoappbackend/todoserver/utils/GlobalExceptionHandlerTestHelperController.kt
@@ -2,11 +2,13 @@ package todoappbackend.todoserver.utils
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import todoappbackend.todoserver.errorhandling.EntityNotFoundException
 
 const val EXCEPTION_HANDLER_TEST_ENDPOINT = "/exception-handler-test"
 
 @Controller
+@RequestMapping(EXCEPTION_HANDLER_TEST_ENDPOINT)
 class GlobalExceptionHandlerTestHelperController {
 
     @GetMapping()
